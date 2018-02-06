a90cae45711b447c919e3d905f217e764805a79b
Luke1982 <gcx3@hotmail.com>
2018-02-06 18:38:19 +0100

    Add toggle for expanding/collapsing the extra information

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("click", handleClicks);
+
+function handleClicks(e) {
+	var functionElement = Utils.findUp(e.target, "data-clickfunction");
+	if (functionElement !== undefined) {
+		switch (functionElement.getAttribute("data-clickfunction")) {
+			case "toggleExtra":
+				Utils.toggleExtra(functionElement, e.target);
+				break;
+		}
+	}
+}
+
+var Utils = {
+	findUp : function(element, searchterm) {
+		while (element = element.parentElement) {
+			if ( (searchterm.charAt(0) === "#" && element.id === searchterm.slice(1) )
+				|| ( searchterm.charAt(0) === "." && element.classList.contains(searchterm.slice(1) ) 
+				|| ( element.hasAttribute(searchterm) ))) {
+				return element;
+			} else if (element == document.body) {
+				break;
+			}
+		}
+	},
+	toggleExtra : function(functionElement, sourceElement) {
+		var lineRoot = this.findUp(functionElement, ".cbds-detail-line");
+		var extraLine = lineRoot.getElementsByClassName("cbds-detail-line__extra")[0];
+		var expandIcon = lineRoot.getElementsByClassName("cbds-exp-coll-icon")[0];
+		extraLine.classList.toggle("cbds-detail-line__extra--expanded");
+		expandIcon.classList.toggle("cbds-exp-coll-icon--expanded");
+	}
+};
