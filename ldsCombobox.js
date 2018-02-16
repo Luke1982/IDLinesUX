878d89d98dac0de0a3a3074ab9faf807de35b1a2
Luke1982 <gcx3@hotmail.com>
2018-02-16 13:47:57 +0100

    Make combobox work with keyboard and mouse

--- ldsCombobox.js
+++ ldsCombobox.js
@@ -16,21 +16,123 @@
 })(function ldscomboboxFactory(){
 
 	function ldsCombobox(el) {
-		el.addEventListener("click", this.handleClick.bind(this));
-
 		this.el 	= el,
-		this.input 	= el.getElementsByClassName("slds-combobox__input")[0];
+		this.input 	= el.getElementsByClassName("slds-combobox__input")[0],
+		this.specialKeys = ["up","down","enter"],
+		this.optionNodes = el.getElementsByClassName("slds-listbox__item"),
+		this.active = false,
+		this.curSel = null,
+		this.curSelIndex = 0;
+
+		/* Instance listeners */
+		_on(el, "click", this.handleClick, this);
+		_on(this.input, "focus", this.trigger, this);
+		_on(this.input, "keyup", this.trigger, this);
+		_on(this.input, "blur", this.close, this);
 	}
 
 	ldsCombobox.prototype = {
-		constructor		: ldsCombobox,
+		constructor: ldsCombobox,
 
-		handleClick		: function(e) {
+		trigger: function(e) {
+			if (!this.active) {
+				this.open();
+			} else {
+				this.handleKeys(e);
+			}
+		},
+
+		open: function() {
+			this.el.classList.add("slds-is-open");
+			this.setOptionState(this.curSelIndex, "selected");
+			this.active = true;
+		},
+
+		close: function() {
+			var _this = this;
+			window.setTimeout(function(){
+				_this.el.classList.remove("slds-is-open");
+				_this.active = false;
+			},200);
+		},
+
+		handleClick: function(e) {
 			var isOption = Utils.findUp(e.target, ".slds-listbox__item");
-			if (isOption)
-				var value = isOption.getElementsByClassName("slds-truncate")[0].innerText;
-				this.input.value = value == undefined ? this.input.value : value;
+			if (isOption != undefined) {
+				this.curSel = isOption.getElementsByClassName("slds-truncate")[0].innerText;
+				this.select();
+			}
+		},
+
+		handleKeys: function(e) {
+			if (this.isSpecialKey(e.keyCode)) {
+				this.handleKey(_getKey(e.keyCode));
+			}
+		},
+
+		handleKey: function(keyName) {
+			switch(keyName) {
+				case "up":
+					this.selectPrevious();
+					break;
+				case "down":
+					this.selectNext();
+					break;
+				case "enter":
+					this.select();
+					break;
+			}
+		},
+
+		isSpecialKey: function(code) {
+			if (window.keycodeMap[code] !== undefined)
+				return this.specialKeys.indexOf(window.keycodeMap[code]) == -1 ? false : true;
+			else
+				return false;
+		},
+
+		setOptionState(index, state) {
+			if (state == "selected") {
+				this.optionNodes[index].children[0].classList.add("slds-has-focus");
+				this.curSel = this.optionNodes[index].getElementsByClassName("slds-truncate")[0].innerText;
+				this.input.value = this.curSel;
+			} else {
+				this.optionNodes[index].children[0].classList.remove("slds-has-focus");
+				this.curSel = "";
+			}
+		},
+
+		selectPrevious: function() {
+			if (this.curSelIndex != 0) {
+				this.setOptionState(this.curSelIndex, "unselected");
+				this.curSelIndex--;
+				this.setOptionState(this.curSelIndex, "selected");
+			}
+		},
+
+		selectNext: function() {
+			if (this.curSelIndex != this.optionNodes.length - 1) {
+				this.setOptionState(this.curSelIndex, "unselected");
+				this.curSelIndex++;
+				this.setOptionState(this.curSelIndex, "selected");
+			}
+		},
+
+		select: function() {
+			this.input.value = this.curSel;
+			this.close();
 		}
+	}
+
+	/**
+	  * Section with factory tools
+	  */
+	function _on(el,type,func,context) {
+		el.addEventListener(type, func.bind(context));
+	}
+
+	function _getKey(code) {
+		return window.keycodeMap[code];
 	}
 
 	return ldsCombobox;
