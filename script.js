b6a5f4b7e0ec1a5388a7dcbda847c46ca47b4bb3
Luke1982 <gcx3@hotmail.com>
2018-02-21 22:11:44 +0100

    Pass param object into InventoryBlock. Fixes #26

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 window.addEventListener("load", function(){
 	var block = document.getElementsByClassName("cbds-inventory-block")[0];
-	window.block = new InventoryBlock(block);
+	window.block = new InventoryBlock(block, {
+			"linesContClass" : "cbds-inventorylines",
+			"lineClass" : "cbds-inventoryline",
+			"linePrefix" : "cbds-inventoryline",
+			"inputPrefix" : "cbds-inventoryline__input"
+	});
 });
