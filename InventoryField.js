dd794d82fa43a5c2bf18a5f85bbd35f6685702cf
Luke1982 <gcx3@hotmail.com>
2018-02-28 12:07:38 +0100

    Format code in _decNum

--- InventoryField.js
+++ InventoryField.js
@@ -208,7 +208,7 @@
 	function _decNum(num) {
 		var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
 		if (!match) { return 0; }
-		return Math.max(0, (match[1] ? match[1].length : 0)	- (match[2] ? +match[2] : 0));
+		return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
 	}
 
 	var keyCodeMap = {
